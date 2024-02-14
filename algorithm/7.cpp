10b89dde52ee6c069a0f736a714e15c24fcdba01
Zeroysx <105535948+Zeroysx@users.noreply.github.com>
2024-02-14 21:11:56 +0800

    1.0.1

--- algorithm/7.cpp
+++ algorithm/7.cpp
@@ -1,21 +1,28 @@
 /*
 插入排序
-
+最后修订时间：2024/2/14
+作者:Wanhe
 */
-//此算法将数组从小到大排列
+//
+/*
+*@return void
+*@param A 要处理的数组
+*@param length 数组的长度
+*@note 插入排序,此算法将数组从小到大排列
+*/
 template <typename T> 
-void insertionSort(T[])
+void insertionSort(T A[],int length)
 {
-    for( int j = 1; j < sizeof(T);j++)
+    for( int j = 1; j < length; j++)
     {
-        int key = T[j];
+        int key = A[j];
         int i = j - 1;
         //比较并插入key
-        while ( i >= 0 && T[i] > key)
+        while ( i >= 0 && A[i] > key)
         {
-            T[i+1] = T[i];
+            A[i+1] = A[i];
             i--;
-        }
+        } 
         A[i+1] = key;    
     }
 }
--- algorithm/7test.cpp
+++ algorithm/7test.cpp
@@ -1,14 +1,19 @@
+/*
+7.cpp测试程序
+最后修订时间：2024/2/14
+作者:Wanhe
+*/
 #include <iostream>
 
 template <typename T> 
-void insertionSort(T A[])
+void insertionSort(T A[],int length)
 {
-    for( int j = 2; j < sizeof(A)/sizeof(A[0]);j++)
+    for( int j = 1; j < length; j++)
     {
         int key = A[j];
         int i = j - 1;
         //比较并插入key
-        while ( i > 0 && A[i] > key)
+        while ( i >= 0 && A[i] > key)
         {
             A[i+1] = A[i];
             i--;
@@ -29,12 +34,13 @@ int main()
     for(int i = 0;i < 5;i++)
         cin>>array[i];
 
+    int length = sizeof(array)/sizeof(array[0]);
     cout<<"Your example:"<<endl;
      for(int i:array)
         cout<<i<<"\t";
     cout<<endl;  
     cout<<"New array:"<<endl;
-    insertionSort(array);
+    insertionSort(array,length);
     for(int i:array)
         cout<<i<<"\t";    
 }
