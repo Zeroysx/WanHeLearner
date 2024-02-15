f2ac9579832ae795a3051ec5c30aadb48a7881a6
Zeroysx <105535948+Zeroysx@users.noreply.github.com>
2024-02-15 20:36:20 +0800

    1.0.2

--- algorithm/10.cpp
+++ algorithm/10.cpp
@@ -3,31 +3,41 @@
 最后修订时间:2024/2/15
 作者:Wanhe
 */
-//结尾标识常量
-const int FLAG=-1;
+//结尾标识常量,取极大值
+const int FLAG=2147483647;
 //形式1：内部排序,指针型
 /*
 *@return void
-*@param array 要排序的数组
 *@param p1 指向第一个有序部分的开头的指针
 *@param p2 指向第一个有序部分的结尾，同时也是第二个有序部分的开头的指针
 *@param p3 指向第二个有序部分的结尾的指针
 *@note 归并排序，此函数将在一个数组中[p1...p2]内有序与[p2,p3]内有序的部分进行排序.
-*@note 注意此函数仅支持正数排序，数组含负数请修改FLAG的值
-*未完成
+*@note 该函数三个指针需指向同一个数组。
 */
 template <typename T>
-void mergeSort(T array,T *p1,T *p2 ,T *p3)
+void mergeSort(T *p1,T *p2 ,T *p3)
 {
     T *arr1 = new T;
     T *arr2 = new T;
     //将两个区间内的值复制
-    int i = 0
-    for(; p1 + i < p2 ; i++)
-        *(arr1+i) = p1 + i;
-    *(arr1+i) =  
-    for(int i = 1; p2 + i < p3 ; i++)
-        *(arr2+i) = p1 + i; 
-    
-
+    int i = 0;
+    for(; p1 + i <= p2 ; i++)
+        *(arr1+i) = *(p1 + i);
+    *(arr1+i) =  FLAG;
+    for(i = 1; p2 + i <= p3 ; i++)
+        *(arr2+i-1) = *(p2 + i);
+    *(arr2+i-1) = FLAG;
+    for(int i = 0,j = 0;p1 <= p3 ;p1++)
+    {
+        if(arr1[i] <= arr2[j])
+          {
+            *p1 = arr1[i];
+            i++;
+        }
+        else 
+        {
+            *p1 = arr2[j];
+            j++;
+        }
+    } 
 }
---
+++ algorithm/10test.cpp
@@ -0,0 +1,59 @@
+/*
+9.cpp测试程序
+最后修订时间：2024/2/15
+作者:Wanhe
+*/
+#include <iostream>
+
+const int FLAG=2147483647;
+
+template <typename T>
+void mergeSort(T *p1,T *p2 ,T *p3)
+{
+    T *arr1 = new T;
+    T *arr2 = new T;
+    //将两个区间内的值复制
+    int i = 0;
+    for(; p1 + i <= p2 ; i++)
+        *(arr1+i) = *(p1 + i);
+    *(arr1+i) =  FLAG;
+    for(i = 1; p2 + i <= p3 ; i++)
+        *(arr2+i-1) = *(p2 + i);
+    *(arr2+i-1) = FLAG;
+    for(int i = 0,j = 0;p1 <= p3 ;p1++)
+    {
+        if(arr1[i] <= arr2[j])
+          {
+            *p1 = arr1[i];
+            i++;
+        }
+        else 
+        {
+            *p1 = arr2[j];
+            j++;
+        }
+    } 
+}
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    cout<<"Input your example(10 int words).The first five numbers and the last five numbers must be in separate order"<<endl;
+    int array[10];
+    //输入样例,前五个数和后五个数必须分别降序
+    for(int i = 0;i < 10;i++)
+        cin>>array[i];
+    //输出原例
+    cout<<"Your example:"<<endl;
+     for(int i:array)
+        cout<<i<<"\t";
+    cout<<endl;
+
+    mergeSort(&array[0],&array[4],&array[9]);  
+    cout<<"New array:"<<endl;
+    for(int i:array)
+        cout<<i<<"\t";    
+}
