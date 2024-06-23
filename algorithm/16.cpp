1c3c3ad05b30dd76bbdf5d8c0312b74be79f572a
Zeroysx <105535948+Zeroysx@users.noreply.github.com>
2024-06-23 09:02:47 +0800

    1.1.5.4

--- algorithm/16.cpp
+++ algorithm/16.cpp
@@ -11,77 +11,117 @@ using std::vector;
 // 定义一个极大数
 const int MAX_NUM = 2147483647;
 
-//Kruskal算法（克鲁斯卡尔算法)
+// Kruskal算法（克鲁斯卡尔算法)
 /*
 基本思路：
 将不同的弧按边权的大小从小到大排序，从小到大开始连接各个端点
 */
-
-// 节点的定义
-typedef struct Node
+typedef struct Tree
 {
-    int id;
-    int val;
+    char val;
+    vector<Tree *> next;
 };
 
-//用一个数组存储以访问的端点
-vector<int> visited_list;
-
-/**
- * @brief 检查节点是否已经被加入树
- * 
- * @param id 端点的唯一的id
- * @param vsted_list 供搜索的已访问列表
- * @return true 已被加入树
- * @return false 未被加入树
- */
-bool ifVisited(int id,vector<int> vsted_list)
+class Max_subtree
 {
-    auto begin = vsted_list.begin();
-    auto end = vsted_list.end();
-    while (begin != end)
-    {
-        if(*begin == id)
-            return true;
-        begin++;
-    }
-    return false;
-}
+private:
+    int length;
+    vector<char> val;
+    // 带边权的邻接矩阵
+    int *adjMatrix;
 
-//边及其边权,此处假设边权全为整数
-typedef struct line_weight
+public:
+    Max_subtree(/* args */);
+    ~Max_subtree();
+    // 为邻接矩阵分配内存
+    void malloc_auto();
+    // 输入端点值
+    void inputval(int value);
+    // 修改邻接矩阵的权值
+    void changematrix(int begin, int end, int weight);
+    // 克鲁斯卡尔算法
+    Tree *Kruskal();
+};
+typedef struct Line
 {
+    int begin;
+    int end;
     int weight;
-    Node node_out;
-    Node node_in;
 };
 
-//使用归并算法对数组进行排序
-/**
- * @brief 归并算法
- * 
- * @param begin 开始位置
- * @param midduim 中间位置
- * @param end 结束位置
- */
-void merge(line_weight* begin,line_weight* midduim,line_weight* end)
+Max_subtree::Max_subtree(/* args */)
 {
-    vector<line_weight> arr1;
-    vector<line_weight> arr2;
-
-    for(int i = 0 ; begin + i < midduim;i++)
-    {
-        arr1.push_back(*(begin + i));
-    }
-    
 }
 
-/**
- * @brief 将弧按照边权从小到大进行排序
- * 
- * @param arr 要排序的边权数组
- */
-void line_sort(line_weight arr[])
+Max_subtree::~Max_subtree()
 {
-    
+}
+
+void Max_subtree::inputval(int value)
+{
+    val.push_back(value);
+    length++;
+}
+void Max_subtree::malloc_auto()
+{
+    delete adjMatrix;
+    adjMatrix = new int[length * length];
+}
+void Max_subtree::changematrix(int begin, int end, int weight)
+{
+    adjMatrix[begin * length + end] = weight;
+}
+
+// 归并排序
+void merge(std::vector<Line>::iterator *begin, std::vector<Line>::iterator *midduim, std::vector<Line>::iterator *end)
+{
+    // copy
+    vector<Line> copy1;
+    vector<Line> copy2;
+    for (int i = 0; begin + i < midduim; i++)
+    {
+        copy1.push_back(*(*(begin + i)));
+        copy2.push_back(*(*(midduim + i)));
+    }
+    // 比较
+    for (int i = 0, j = 0; begin != end; begin++)
+    {
+        if (copy1[i].weight > copy2[j].weight)
+        {
+            **begin = copy1[i];
+            i++;
+        }
+        else
+        {
+            **begin = copy2[j];
+            j++;
+        }
+    }
+}
+void mergeSort(std::vector<Line> *begin)
+{
+    if (!begin->empty())
+    {
+        
+    }
+}
+//
+Tree *Max_subtree::Kruskal()
+{
+    // 将所有边压入数组中
+    vector<Line> arrLine;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            if (adjMatrix[i * length + j] != 0)
+            {
+                arrLine.push_back({i, j, adjMatrix[i * length + j]});
+            }
+        }
+    }
+
+    // 将这些边按权值从小到大排序
+    auto begin = arrLine.begin();
+    auto end = arrLine.end();
 }
